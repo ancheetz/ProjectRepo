46180ccab0086f244349542f1cf44153a8e6516e
Andrea Czarnecki <ancheetz@me.com>
2020-06-21 23:12:06 -0400

    Bucket List app start

--- bucket_list/app.py
+++ bucket_list/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to my page!"
-
-
+    return render_template("index.html")
 
 
 
